2452a4a06a4dcd44bdadd9c12190ec99950ee431
Lucas-Gardini <lucasgardini.dias@hotmail.com>
2025-09-23 22:59:45 -0300

    feat: implementar configuração inicial do frontend com Nuxt.js
    feat: adicionar configuração de cores no app.config.ts
    feat: criar página inicial com estrutura básica em index.vue
    style: organizar e importar estilos no main.css
    feat: adicionar suporte a sass-embedded nas dependências

---
+++ email-classifier.frontend/app/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		Ideia:<br></br>
+			Lado esquerdo: Campos para: subject,sender,body<br></br>
+			Lado direito: Classificação, resumo e sugestão de resposta<br></br>
+			Ficar fazendo pooling a cada X segundos pelo ID (precisa implementar a rota)<br></br>
+<br></br>
+			Listagem de emails<br></br>
+	</div>
+</template>
