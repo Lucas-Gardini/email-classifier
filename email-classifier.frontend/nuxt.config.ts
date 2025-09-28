4acd7e42987a82c012ccb674a5ec72acde859635
Lucas-Gardini <lucasgardini.dias@hotmail.com>
2025-09-28 15:19:29 -0300

    refact: remover baseURL do nuxt.config.ts
    - Eliminar a propriedade baseURL que não estava sendo utilizada.

--- email-classifier.frontend/nuxt.config.ts
+++ email-classifier.frontend/nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	app: {
-		baseURL: "/email-classifier/",
 		head: {
 			titleTemplate: "Email Classifier",
 			htmlAttrs: { lang: "pt-BR" },
